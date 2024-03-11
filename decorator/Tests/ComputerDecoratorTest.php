d18ad095675b181257f9d4063017a78974829f3d
Timeuh <timeuuh@gmail.com>
2024-03-11 17:10:39 +0100

    :white_check_mark: implémente les tests

--- decorator/Tests/ComputerDecoratorTest.php
+++ decorator/Tests/ComputerDecoratorTest.php
@@ -2,6 +2,8 @@
 
 namespace Test;
 
+use App\GPU;
+use App\OLEDScreen;
 use PHPUnit\Framework\TestCase;
 
 use App\Laptop;
@@ -11,20 +13,36 @@ class ComputerDecoratorTest extends TestCase
     public function testBasicLaptop()
     {
         $laptop = new Laptop();
-        
+
         $this->assertSame(400, $laptop->getPrice());
         $this->assertSame("A laptop computer", $laptop->getDescription());
     }
 
     public function testLaptopWithGPU()
     {
-        // TODO: faire le test
-        $this->assertSame(true, 1 === 1);
+        $laptop = new Laptop();
+        $laptopWithGPU = new GPU($laptop);
+
+        $this->assertSame(600, $laptopWithGPU->getPrice());
+        $this->assertSame("A laptop computer, with a GPU", $laptopWithGPU->getDescription());
     }
 
     public function testLaptopWithOLEDScreen()
     {
-        // TODO: faire le test
-        $this->assertSame(false, 1 === 2);
+        $laptop = new Laptop();
+        $laptopWithOLEDScreen = new OLEDScreen($laptop);
+
+        $this->assertSame(700, $laptopWithOLEDScreen->getPrice());
+        $this->assertSame("A laptop computer, with an OLED screen", $laptopWithOLEDScreen->getDescription());
+    }
+
+    public function testLaptopWithGPUWithOLEDWScreen()
+    {
+        $laptop = new Laptop();
+        $laptopWithOLEDScreen = new OLEDScreen($laptop);
+        $laptopWithOLEDScreenAndGPU = new GPU($laptopWithOLEDScreen);
+
+        $this->assertSame(900, $laptopWithOLEDScreenAndGPU->getPrice());
+        $this->assertSame("A laptop computer, with an OLED screen, with a GPU", $laptopWithOLEDScreenAndGPU->getDescription());
     }
 }
