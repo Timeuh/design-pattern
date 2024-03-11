f832520785cf6995004ef296d644f81dddd36af5
Timeuh <timeuuh@gmail.com>
2024-03-11 15:36:40 +0100

    :sparkles: teste les méthodes de factory

--- factory/public/index.php
+++ factory/public/index.php
@@ -1,4 +1,18 @@
 <?php
+
+use App\Entity\VehicleType;
+
 require('../vendor/autoload.php');
 
-# Essayer d'utiliser votre factory ici
+$factory = new App\Factory\VehicleFactory();
+
+$bicycle = $factory->create(VehicleType::BICYCLE, 0.0, 'muscle');
+$car = $factory->create(VehicleType::CAR, 0.5, 'essence');
+$truck = $factory->create(VehicleType::TRUCK, 2.5, 'essence');
+
+print_r($bicycle);
+print_r($car);
+print_r($truck);
+
+$vehicle = $factory->createByDistanceAndWeight(10, 15);
+print_r($vehicle);
