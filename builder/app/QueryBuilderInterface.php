4e361ebdaf53abd79332d07c57c5f00cec96f6f2
Timeuh <timeuuh@gmail.com>
2024-03-11 16:19:21 +0100

    :sparkles: crée l'interface de query builder

--- builder/app/QueryBuilderInterface.php
+++ builder/app/QueryBuilderInterface.php
@@ -1,5 +1,10 @@
 <?php
 
-# TODO: Créer une classe QueryBuilder en utilisant le design pattern Builder
-
 namespace App;
+
+interface QueryBuilderInterface {
+    public function select(string $fields): QueryBuilderInterface;
+    public function from(string $table): QueryBuilderInterface;
+    public function where(string $condition): QueryBuilderInterface;
+    public function getQuery(): string;
+}
